9b0d9bab69d3ca301bafc91508295aad7ea51495
chevrette <denkiewicz.karolina@gmail.com>
2019-04-08 18:06:12 +0200

    Improve Options component

--- review-notifications/src/App.js
+++ review-notifications/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import { Github } from './Github';
-import { Options } from './Options';
+import Options from './Options';
 import './App.css';
 
 class App extends Component {
